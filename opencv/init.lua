6f7b299461c39f9cb7e4ee6599b155c329f61f7c
Clement Farabet <clement.farabet@gmail.com>
2011-12-09 22:20:32 -0500

    Alowing multiple instances of cameras, and files.

--- opencv/init.lua
+++ opencv/init.lua
@@ -14,29 +14,39 @@ local Camera = torch.class('image.Camera')
 
 function Camera:__init(...)
    -- parse args
-   local args, idx = xlua.unpack(
+   local args, idx, width, height = xlua.unpack(
       {...},
       'image.Camera', nil,
-      {arg='idx', type='number', help='camera index', default=0}
+      {arg='idx', type='number', help='camera index', default=0},
+      {arg='width', type='number', help='frame width', default=640},
+      {arg='height', type='number', help='frame height', default=480}
    )
+
    -- init vars
-   self.height = 480
-   self.width = 640
-   self.camidx = (idx or -1);
-   self.tensor = torch.DoubleTensor(3,self.height,self.width)
-   self.tensortyped = torch.Tensor()
+   self.tensorsized = torch.FloatTensor(3, height, width)
+   self.buffer = torch.FloatTensor()
+   self.tensortyped = torch.Tensor(3, height, width)
 
    -- init capture
-   libcamopencv.initCam(idx);
+   self.idx = libcamopencv.initCam(idx, width, height)
 end
 
 function Camera:forward()
-   libcamopencv.grabFrame(self.tensor)
-   self.tensortyped:resize(3,self.tensor:size(2),self.tensor:size(3)):copy(self.tensor)
+   libcamopencv.grabFrame(self.idx, self.buffer)
+   if self.tensorsized:size(2) ~= self.buffer:size(2) or self.tensorsized:size(3) ~= self.buffer:size(3) then
+      image.scale(self.buffer, self.tensorsized)
+   else
+      self.tensorsized = self.buffer
+   end
+   if self.tensortyped:type() ~= self.tensorsized:type() then
+      self.tensortyped:copy(self.tensorsized)
+   else
+      self.tensortyped = self.tensorsized
+   end
    return self.tensortyped
 end
 
 function Camera:stop()
-  libcamopencv.releaseCam()
+  libcamopencv.releaseCam(self.idx)
   print('stopping camera')
 end
--- opencv/opencv.c
+++ opencv/opencv.c
@@ -29,64 +29,91 @@
 #include <cv.h>
 #include <highgui.h>
 
-static CvCapture* capture;
-static IplImage* frame;
+#define MAXIDX 100
+static CvCapture* capture[MAXIDX];
+static IplImage* frame[MAXIDX];
+static fidx = 0;
 
 static int l_initCam(lua_State *L) {
-  const int idx = lua_tonumber(L, 1);
-  printf("initializing camera\n");
-  capture = cvCreateCameraCapture(idx);
-  if( capture == NULL ) {
-    perror("could not create OpenCV capture");
+  // args
+  int width = lua_tonumber(L, 2);
+  int height = lua_tonumber(L, 3);
+
+  // max allocs ?
+  if (fidx == MAXIDX) {
+    perror("max nb of devices reached...\n");
   }
-  sleep(2);
-  cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640);
-  cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);
-  frame = cvQueryFrame ( capture );
-  int tries = 8;
-  while ((!frame || frame->height != 480 || frame->width != 640) && tries>0) {
-    frame = cvQueryFrame ( capture );
-    tries--;
+
+  // if number, open a camera device
+  if (lua_isnumber(L, 1)) {
+    printf("initializing camera\n");
+    const int idx = lua_tonumber(L, 1);
+    capture[fidx] = cvCreateCameraCapture(idx);
+    if( capture[fidx] == NULL ) {
+      perror("could not create OpenCV capture");
+    }
+    sleep(2);
+    cvSetCaptureProperty(capture[fidx], CV_CAP_PROP_FRAME_WIDTH, width);
+    cvSetCaptureProperty(capture[fidx], CV_CAP_PROP_FRAME_HEIGHT, height);
+    frame[fidx] = cvQueryFrame ( capture[fidx] );
+    int tries = 50;
+    while ((!frame[fidx] || frame[fidx]->height != height || frame[fidx]->width != width) && tries>0) {
+      frame[fidx] = cvQueryFrame ( capture[fidx] );
+      tries--;
+    }
+    if ( frame[fidx] == NULL ) {
+      perror("failed OpenCV test capture");
+    }
+    if(frame[fidx]->depth != IPL_DEPTH_8U) {
+      perror("initCam: opencv supported for 8-bit unsigned capture only");
+    }
+    printf("camera initialized\n");
+  } else {
+    // open a file;
+    const char *file = lua_tostring(L, 1);
+    printf("initializing frame grabber on file: %s\n", file);
+    capture[fidx] = cvCreateFileCapture(file);
+    if( capture[fidx] == NULL ) {
+      perror("could not create OpenCV capture");
+    }
   }
-  if ( frame == NULL ) {
-    perror("failed OpenCV test capture");
-  }
-  if(frame->depth != IPL_DEPTH_8U) {
-    perror("initCam: opencv supported for 8-bit unsigned capture only");
-  }
-  printf("camera initialized\n");
+
+  // next
+  lua_pushnumber(L, fidx);
+  fidx ++;
   return 0;
 }
 
 // frame grabber
 static int l_grabFrame (lua_State *L) {
   // Get Tensor's Info
-  THDoubleTensor * tensor = luaT_checkudata(L, 1, luaT_checktypename2id(L, "torch.DoubleTensor"));
+  const int idx = lua_tonumber(L, 1);
+  THFloatTensor * tensor = luaT_checkudata(L, 2, luaT_checktypename2id(L, "torch.FloatTensor"));
 
   // grab frame
-  frame = cvQueryFrame ( capture );
-  if( !frame ) {
+  frame[idx] = cvQueryFrame ( capture[idx] );
+  if( !frame[idx] ) {
     perror("could not query OpenCV capture");
   }
 
   // resize given tensor
-  THDoubleTensor_resize3d(tensor, 3, frame->height, frame->width);
+  THFloatTensor_resize3d(tensor, 3, frame[idx]->height, frame[idx]->width);
 
   // copy to tensor
   int m0 = tensor->stride[1];
   int m1 = tensor->stride[2];
   int m2 = tensor->stride[0];
-  unsigned char *src = frame->imageData;
-  double *dst = THDoubleTensor_data(tensor);
+  unsigned char *src = frame[idx]->imageData;
+  float *dst = THFloatTensor_data(tensor);
   int i, j, k;
-  for (i=0; i < frame->height; i++) {
-    for (j=0, k=0; j < frame->width; j++, k+=m1) {
+  for (i=0; i < frame[idx]->height; i++) {
+    for (j=0, k=0; j < frame[idx]->width; j++, k+=m1) {
       // red:
-      dst[k] = src[i*frame->widthStep + j*frame->nChannels + 2]/255.;
+      dst[k] = src[i*frame[idx]->widthStep + j*frame[idx]->nChannels + 2]/255.;
       // green:
-      dst[k+m2] = src[i*frame->widthStep + j*frame->nChannels + 1]/255.;
+      dst[k+m2] = src[i*frame[idx]->widthStep + j*frame[idx]->nChannels + 1]/255.;
       // blue:
-      dst[k+2*m2] = src[i*frame->widthStep + j*frame->nChannels + 0]/255.;
+      dst[k+2*m2] = src[i*frame[idx]->widthStep + j*frame[idx]->nChannels + 0]/255.;
     }
     dst += m0;
   }
@@ -95,7 +122,8 @@ static int l_grabFrame (lua_State *L) {
 }
 
 static int l_releaseCam (lua_State *L) {
-  cvReleaseCapture( &capture );
+  const int idx = lua_tonumber(L, 1);
+  cvReleaseCapture( &capture[idx] );
   return 0;
 }
 
